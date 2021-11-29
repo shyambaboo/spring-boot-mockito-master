efbddf2730943a828520d1d0ddf5eda817f47b60
shyambaboo <shyambaboogupta@gmail.com>
2021-11-29 23:25:10 +0530

    Update Constant.java

--- src/main/java/com/javatechie/spring/mockito/api/Constant.java
+++ src/main/java/com/javatechie/spring/mockito/api/Constant.java
@@ -5,4 +5,5 @@ public class Constant {
 	public static final String TEST = "test";
 	public static final String HELLO = "hello";
 	public static final String EXAMPLE = "example";
+	public static final String EXAMPLE = "example";
 }
